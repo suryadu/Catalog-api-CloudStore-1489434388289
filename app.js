8ed9e7a9c4ff5166a0e7c784b6acd9a46780cc0e
Snehal Pansare <snehalpansare@Snehals-MacBook-Pro.local>
2017-02-20 13:51:31 +0530

    check-in files

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var cfenv = require("cfenv");
+var path = require('path');
+var cors = require('cors');
+var bodyParser = require('body-parser');
+//var sd = require('./service-discovery/service-discovery.js');
+
+var appEnv = cfenv.getAppEnv();
+
+// Setup middleware
+var app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'www')));
+
+// Setup Service Discovery
+//sd.register();
+
+// Require the API routes.
+require("./routes/routes.js")(app);
+
+app.listen(appEnv.port, appEnv.bind);
+console.log('App started on ' + appEnv.bind + ':' + appEnv.port);
